66d8bb80935c0f9ccf425a638de168e0d4af4e64
Jing Liu <jingliu778@gmail.com>
2022-02-25 22:03:32 -0500

    update

--- __test__/employee.test.js
+++ __test__/employee.test.js
@@ -1,30 +1,37 @@
-const Employee = require('../lib/employee');
+const Employee = require("../lib/employee");
 
-describe("Emolyee class", ()=>{
-    it("Create a employee object ",()=>{
-        const employee = new Employee('Jared', 1, 'jared@fakemail.com');
-        expect(employee.name).toBe('Jared');
-        expect(employee.id).toBe(1);
-        expect(employee.email).toBe('jared@fakemail.com')
+describe("Emolyee class", () => {
+  it("Create a employee object ", () => {
+    const employee = new Employee("Jared", 1, "jared@fakemail.com");
+    expect(employee.name).toBe("Jared");
+    expect(employee.id).toBe(1);
+    expect(employee.email).toBe("jared@fakemail.com");
+  });
+  describe("getName method", () => {
+    it("return name", () => {
+      expect(new Employee("Jared", 1, "jared@fakemail.com").getName()).toBe(
+        "Jared"
+      );
     });
-     describe("getName method",()=>{
-         it('return name', ()=>{
-             expect(new Employee('Jared', 1, 'jared@fakemail.com').getName()).toBe('Jared')
-         })
-     });
-     describe("getId method",()=>{
-        it('return id', ()=>{
-            expect(new Employee('Jared', 1, 'jared@fakemail.com').getId()).toBe(1)
-        })
+  });
+  describe("getId method", () => {
+    it("return id", () => {
+      expect(new Employee("Jared", 1, "jared@fakemail.com").getId()).toBe(1);
     });
-    describe("getEmail method",()=>{
-        it('return email', ()=>{
-            expect(new Employee('Jared', 1, 'jared@fakemail.com').getEmail()).toBe('jared@fakemail.com')
-        })
+  });
+  describe("getEmail method", () => {
+    it("return email", () => {
+      expect(new Employee("Jared", 1, "jared@fakemail.com").getEmail()).toBe(
+        "jared@fakemail.com"
+      );
     });
-    describe("getRole method",()=>{
-        it('return Employee', ()=>{
-            expect(new Employee('Jared', 1, 'jared@fakemail.com').getRole()).toBe('Employee')
-        })
+  });
+
+  describe("getRole method", () => {
+    it("return Employee", () => {
+      expect(new Employee("Jared", 1, "jared@fakemail.com").getRole()).toBe(
+        "Employee"
+      );
     });
-})
+  });
+});
